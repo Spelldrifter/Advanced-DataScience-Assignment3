b01bd22d192dc31cc764948179c65b64aa3aa1bf
Spelldrifter <spelldrifter@proton.me>
2022-10-22 19:55:03 +0000

    Bump version: 1.2.0 → 1.2.1

--- custom_reduce_filter.py
+++ custom_reduce_filter.py
@@ -4,3 +4,5 @@ Problem Statement​ ​1:
 Write a Python Program to implement your own myreduce() function which works exactly
 like Python's built-in function reduce()
 Problem Statement​ ​2:
+Write a Python program to implement your own myfilter() function which works exactly
+like Python's built-in function filter()
