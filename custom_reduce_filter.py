c81c5628a3927a8fb19a4924a7f72c6d44da4d82
Spelldrifter <spelldrifter@proton.me>
2022-11-02 20:26:37 +0000

    update fixme

--- custom_reduce_filter.py
+++ custom_reduce_filter.py
@@ -29,3 +29,6 @@ def myfilter(anyfunc, sequence):
  # iterate over sequence of items in sequence and apply filter function
  for item in sequence:
   if anyfunc(item):
+   result.append(item)
+
+ # return funal output
